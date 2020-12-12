ae7541a6ba3f93a63bbaa46d2ecfba3ce53ac0f4
Bjorn Sydenham <bjorn.sydenham@thetrainline.com>
2020-12-12 16:09:26 +0000

    Using private methods

--- ConstructionLine.CodingChallenge/SearchEngine.cs
+++ ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ConstructionLine.CodingChallenge
@@ -6,14 +7,12 @@ namespace ConstructionLine.CodingChallenge
     public class SearchEngine
     {
         private readonly List<Shirt> _shirts;
-        private List<Shirt> _matchingShirts;
         private readonly List<SizeCount> _sizeCounts;
         private readonly List<ColorCount> _colorCounts;
+
         public SearchEngine(List<Shirt> shirts)
         {
             _shirts = shirts;
-
-            _matchingShirts = new List<Shirt>();
 
             _sizeCounts = Size.All.Select(size => new SizeCount
             {
@@ -34,43 +33,60 @@ namespace ConstructionLine.CodingChallenge
             var optionSizeIds = options.Sizes.Select(size => size.Id).ToList();
             var optionsColorIds = options.Colors.Select(color => color.Id).ToList();
 
-            if (optionSizeIds.Any() && optionsColorIds.Any())
-            {
-                _matchingShirts = _shirts.Where(shirt => optionSizeIds.Contains(shirt.Size.Id) && optionsColorIds.Contains(shirt.Color.Id)).ToList();
-            }
-            else if (!optionSizeIds.Any() && optionsColorIds.Any())
-            {
-                _matchingShirts = _shirts.Where(shirt => optionsColorIds.Contains(shirt.Color.Id)).ToList();
-            }
-            else if (optionSizeIds.Any() && !optionsColorIds.Any())
-            {
-                _matchingShirts = _shirts.Where(shirt => optionSizeIds.Contains(shirt.Size.Id)).ToList();
-            }
-            else
-            {
-                _matchingShirts = _shirts;
-            }
+            var matchingShirts = GetMatchingShirts(optionSizeIds, optionsColorIds);
 
-            foreach (var size in options.Sizes)
-            {
-                var sizeCountToChange = _sizeCounts.Single(sizeCount => sizeCount.Size.Equals(size));
-
-                sizeCountToChange.Count = _matchingShirts.Count(s => s.Size.Id == size.Id && (!options.Colors.Any() || optionsColorIds.Contains(s.Color.Id)));
-            }
-
-            foreach (var color in options.Colors)
-            {
-                var colorCountToChange = _colorCounts.Single(colorCount => colorCount.Color.Equals(color));
-
-                colorCountToChange.Count = _matchingShirts.Count(c => c.Color.Id == color.Id && (!options.Sizes.Any() || optionSizeIds.Contains(c.Size.Id)));
-            }
+            ChangeSizeCounts(optionSizeIds, matchingShirts);
+            ChangeColorCounts(optionsColorIds, matchingShirts);
 
             return new SearchResults
             {
-                Shirts = _matchingShirts,
+                Shirts = matchingShirts,
                 SizeCounts = _sizeCounts,
                 ColorCounts = _colorCounts
             };
+        }
+
+        private List<Shirt> GetMatchingShirts(List<Guid> optionSizeIds, List<Guid> optionColorIds)
+        {
+            var anySizeOptions = optionSizeIds.Any();
+            var anyColorOptions = optionColorIds.Any();
+
+            if (anySizeOptions && anyColorOptions)
+            {
+                return _shirts.Where(shirt => optionSizeIds.Contains(shirt.Size.Id) && optionColorIds.Contains(shirt.Color.Id)).ToList();
+            }
+
+            if (!anySizeOptions && anyColorOptions)
+            {
+                return _shirts.Where(shirt => optionColorIds.Contains(shirt.Color.Id)).ToList();
+            }
+
+            if (anySizeOptions && !anyColorOptions)
+            {
+                return _shirts.Where(shirt => optionSizeIds.Contains(shirt.Size.Id)).ToList();
+            }
+
+            return _shirts;
+        }
+
+        private void ChangeSizeCounts(List<Guid> optionSizeIds, List<Shirt> matchingShirts)
+        {
+            foreach (var sizeId in optionSizeIds)
+            {
+                var sizeCountToChange = _sizeCounts.Single(sizeCount => sizeCount.Size.Id.Equals(sizeId));
+
+                sizeCountToChange.Count = matchingShirts.Count(s => s.Size.Id == sizeId);
+            }
+        }
+
+        private void ChangeColorCounts(List<Guid> optionColorIds, List<Shirt> matchingShirts)
+        {
+            foreach (var colorId in optionColorIds)
+            {
+                var colorCountToChange = _colorCounts.Single(colorCount => colorCount.Color.Id.Equals(colorId));
+
+                colorCountToChange.Count = matchingShirts.Count(c => c.Color.Id == colorId);
+            }
         }
     }
 }
